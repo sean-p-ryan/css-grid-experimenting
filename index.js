f09f0001be4f3530c03fa3cc92a8c747db71d5f5
Sean Ryan <ryan.sean@acmhq.acm.org>
2019-06-07 17:34:47 -0400

    added more logic

---
+++ index.js
@@ -0,0 +1,8 @@
+let button = document.querySelector("#trigger");
+let squares = document.querySelectorAll("#square")
+
+button.addEventListener('click', changeSquareColors());
+
+const changeSquareColors = () => {
+
+}
