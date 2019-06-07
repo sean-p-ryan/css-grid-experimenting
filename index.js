6f7d60a2ad8f061a9f9f380fda1ee711e53b5f21
Sean Ryan <ryan.sean@acmhq.acm.org>
2019-06-07 17:35:28 -0400

    some more JS added\

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 let button = document.querySelector("#trigger");
-let squares = document.querySelectorAll("#square")
+let squaresArray = Array.prototype.slice.call(document.querySelectorAll("#square"));
+let squaresChangingColor = false;
 
 button.addEventListener('click', changeSquareColors());
 
 const changeSquareColors = () => {
+    // iterate through squares array and set background color of each to random color
+}
 
+const generateRandomColor = () => {
+    // return random
 }
