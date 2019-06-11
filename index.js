ee73f2cbd3438c7824771fc53f9d37ca3ac15b99
Sean Ryan <ryan.sean@acmhq.acm.org>
2019-06-11 12:28:11 -0400

    squares changing color now

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ let button = document.querySelector("#trigger");
 let squaresArray = Array.prototype.slice.call(document.querySelectorAll("#square"));
 let squaresChangingColor = false;
 
-button.addEventListener('click', changeSquareColors());
-
 const changeSquareColors = () => {
-    // iterate through squares array and set background color of each to random color
+    if (squaresChangingColor) {
+        setTimeout(() => {
+            squaresArray.forEach(square => {
+                square.style.backgroundColor = generateRandomColor();
+            })
+            changeSquareColors();
+        }, 200)
+    }
+}
+
+const setSquaresChangingColor = () => {
+    (squaresChangingColor) ?
+        squaresChangingColor = false :
+        squaresChangingColor = true;
+    // call changeSquareColors
+    changeSquareColors();
 }
 
 const generateRandomColor = () => {
-    // return random
+    // return random color
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    return "#" + randomColor;
 }
+
+button.addEventListener('click', setSquaresChangingColor);
